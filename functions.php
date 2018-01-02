2918666a2fde36c689dc0daaf4d9cf85fe9793a1
Þórarinn Ingi Tómasson <1thorarinn@gmail.com>
2018-01-02 23:14:59 +0000

    Try remove jquery

--- functions.php
+++ functions.php
@@ -21,8 +21,8 @@ require 'vendor/autoload.php';
 
 
 function theme_js() {
-    wp_deregister_script('jquery');
-    wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', array(), null, false);
+    //wp_deregister_script('jquery');
+    //wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
     wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', '' );
      wp_enqueue_script( 'custom', get_template_directory_uri() . '/assets/js/custom.js', array( "jquery", "wp-util" ), '1.0', true );
 
